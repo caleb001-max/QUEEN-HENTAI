4840ba71b69ab3e4d0e53e7b342c77f11f220482
CYBER®GHOST083 <155654330+caleb001-max@users.noreply.github.com>
2024-04-04 01:25:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939688', 'Dinuwa Official²⁰²³', true], 
+  ['94767939688', 'sass', true],
+  ['94767939688', 'sass', true], 
   [''] 
 ] //Number of owners
 
